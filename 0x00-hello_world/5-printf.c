6e04d4858151e7c0fe30885fae3e3e2cd777dc22
NiceCreativity <unlimitech1@gmail.com>
2023-03-16 19:13:52 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print the f function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+print("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
