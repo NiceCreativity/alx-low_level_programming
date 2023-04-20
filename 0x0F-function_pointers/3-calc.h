6e8e776b02ce1a3020a36c890564ecadbd48a782
NiceCreativity <unlimitech1@gmail.com>
2023-04-19 19:32:55 -0700

    3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,82 +1,21 @@
-#include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
-
-
+#ifndef CALC_H
+#define CALC_H
 /**
-* op_add - ...
-* @a: ...
-* @b: ...
-*
-* Return: ...
-*/
-int op_add(int a, int b)
+ * struct op - A struct op.
+ * @op: The operator.
+ * @f: The associated function.
+ */
+typedef struct op
 {
-   return (a + b);
-}
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
-/**
-* op_sub - ...
-* @a: ...
-* @b: ...
-*
-* Return: ...
-*/
-int op_sub(int a, int b)
-{
-   return (a - b);
-}
-
-
-/**
-* op_mul - ...
-* @a: ...
-* @b: ...
-*
-* Return: ...
-*/
-int op_mul(int a, int b)
-{
-   return (a * b);
-}
-
-
-/**
-* op_div - ...
-* @a: ...
-* @b: ...
-*
-* Return: ...
-*/
-int op_div(int a, int b)
-{
-   if (b == 0)
-   {
-       printf("Error\n");
-       exit(100);
-   }
-
-
-   return (a / b);
-}
-
-
-/**
-* op_mod - ...
-* @a: ...
-* @b: ...
-*
-* Return: ...
-*/
-int op_mod(int a, int b)
-{
-   if (b == 0)
-   {
-       printf("Error\n");
-       exit(100);
-   }
-
-
-   return (a % b);
-}
+#endif
